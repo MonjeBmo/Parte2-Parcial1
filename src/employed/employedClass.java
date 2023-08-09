6de3443d2a10b521cb884d862062179e1ea92abb
Victor Monje <monjev086@gmail.com>
2023-08-09 13:04:57 -0600

    fix : add comments class Employed

--- src/employed/employedClass.java
+++ src/employed/employedClass.java
@@ -1,24 +1,33 @@
 package employed;
 
+// Clase abstracta que representa a un empleado
 public abstract class EmployedClass {
 
+    // Atributos privados que encapsulan los datos del empleado
     private int id;
     private String name;
     private String lastname;
     private double salary;
 
-    public int countEmployed;
+    // Variable de clase para contar empleados (encapsulación y uso de variable de clase)
+    public static int countEmployed;
 
+    // Método abstracto para calcular el salario (abstracción)
     public abstract double calculateSalary();
 
+    // Constructor de la clase que inicializa los atributos (encapsulación)
     public EmployedClass(int id, String name, String lastname, double salary) {
+        // Incrementar el contador de empleados
         countEmployed += 1;
+        
+        // Inicializar los atributos
         this.id = id;
         this.name = name;
         this.lastname = lastname;
         this.salary = salary;
     }
 
+    // Métodos getter y setter para acceder y modificar los atributos (encapsulación)
     public int getId() {
         return id;
     }
@@ -50,5 +59,4 @@ public abstract class EmployedClass {
     public void setSalary(double salary) {
         this.salary = salary;
     }
-
 }
